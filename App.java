c98d96c75e33f7ca2ba705182db73edd1c08a4c6
Yohandaa <89744011+Yohandaa@users.noreply.github.com>
2023-11-29 20:52:16 +0700

    Add files via upload

---
+++ App.java
@@ -0,0 +1,21 @@
+package com.PBO11;
+
+public class App {
+    public static void main(String[] args) {
+
+        //polymorphisme
+        DataPelanggan P01 = new Pelanggan();
+ 
+    //exception    
+    try {
+        P01.isiDataPelanggan();
+    } 
+    catch (Exception e){
+        System.out.println("Terjadi salah input");
+    }
+
+        //mencetak struk
+        P01.cetakStruk();
+
+    }
+}
---
+++ DataPelanggan.java
@@ -0,0 +1,20 @@
+package com.PBO11;
+
+public class DataPelanggan implements Struk {
+    String faktur;
+    String nama;
+    String namaBarang;
+    Integer hargaBarang;
+    Integer jmlBarang;
+    Integer totalBayar;
+
+    @Override
+    public void cetakStruk() {
+    }
+
+    //method untuk mengisi data pelanggan
+    public void isiDataPelanggan() {
+    }
+
+    
+}
